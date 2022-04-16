5edd2e78de0a5db5580edd1659e03660dc995566
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-16 16:20:55 -0300

    init App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
 
-function App() {
+const App = () => {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div>
-			<p className="text-red-400">Hello</p>
-		</div>
+		<>
+			<SectionWrapper
+				title="Your own store of amazing NFTs. Simple, fast, and secure."
+				description="Buy, store, exchange and earn crypto. Join 5+ million people using ProNef"
+				showBtn
+				mockupImg={assets.homeHero}
+				banner="banner"
+			/>
+		</>
 	);
-}
+};
 
 export default App;
