ec4f401f26570b6db886fbb04f6282a02cc2bd31
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-16 17:50:59 -0300

    started features component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import assets from "../assets";
+import styles from "../styles/Global";
 
 const Features = () => {
-	return <div>Features</div>;
+	return (
+		<div className={`${styles.section} ${styles.bgPrimary} banner03`}>
+			<div className={`${styles.subSection} flex-col text-center`}>
+				<div>
+					<h1 className={``}>Technologies</h1>
+					<p>
+						ProNef has been developed using a cross-platform technology, React
+						Native.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Features;
