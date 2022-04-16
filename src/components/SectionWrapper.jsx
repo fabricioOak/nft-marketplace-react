d3e6d336fde1ce876763d4cca9ff9ecc12a4c786
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-16 16:21:25 -0300

    component usability update

---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../styles/Global";
+import assets from "../assets";
+import Button from "./Button.jsx";
+
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+	return (
+		<div
+			className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}
+		>
+			<div className={`flex items-center ${styles.boxClass}`}>
+				<div className={`${styles.descDiv}`}>
+					<h1 className={`${styles.h1Text}`}>{title}</h1>
+					<p className={`${styles.descriptionText}`}>{description}</p>
+					{showBtn && <Button assetUrl={assets.expo} link="Link to the App" />}
+				</div>
+				<div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+					<img src={mockupImg} alt="Hero Image" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SectionWrapper;
