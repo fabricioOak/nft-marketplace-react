efc6e331571118651ee0c665ca32674b40436e0f
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-17 00:00:59 -0300

    download section

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
+import styles from "../styles/Global";
+import assets from "../assets";
+
 const Download = () => {
-	return <div>Download</div>;
+	return (
+		<div className={`${styles.section} ${styles.bgWhite}`}>
+			<div className={`${styles.subSection} flex-col text-center`}>
+				<div>
+					<h1 className={`${styles.h1Text} ${styles.blackText}`}>
+						Download the Source Code
+					</h1>
+					<p className={`${styles.pText} ${styles.blackText}`}>
+						Get the full source code on GitHub
+					</p>
+				</div>
+				<button
+					onClick={() => {
+						window.open(
+							"https://github.com/fabricioOak/nft-marketplace-react-native",
+							"_blank"
+						);
+					}}
+					className={`${styles.btnPrimary}`}
+				>
+					Source Code
+				</button>
+				<div className={`${styles.flexCenter}`}>
+					<img
+						className={styles.fullImg}
+						src={assets.scene}
+						alt="download-png"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Download;
