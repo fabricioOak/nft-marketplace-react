fc2091e4dc3bdd865d8f7ba345d5532ae73768ce
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-17 00:02:22 -0300

    link to expo

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -41,7 +41,12 @@ const SectionWrapper = ({
 					>
 						{description}
 					</p>
-					{showBtn && <Button assetUrl={assets.expo} link="Link to the App" />}
+					{showBtn && (
+						<Button
+							assetUrl={assets.expo}
+							link="https://expo.dev/@fabriciooak/nft-marketplace-rn-app?serviceType=classic&distribution=expo-go"
+						/>
+					)}
 				</div>
 				<div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
 					<img
